c6a7e7ffad88bc617dbe08be4d2b5d05a2f9b4ab
PauloPinto87 <paulo@example.com>
2025-11-15 22:38:34 +0000

    Melhorar contraste toggle, remover clipping, adicionar 10 máquinas

--- src/data/machines.js
+++ src/data/machines.js
@@ -43,4 +43,74 @@ export const machines = [
     imageUrl:
       "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/BO5041_C2L0.jpg",
   },
+  {
+    id: 7,
+    name: "Berbequim sem fios",
+    reference: "DCD777C2",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DCD777C2_C2L0.jpg",
+  },
+  {
+    id: 8,
+    name: "Serra de vaivém sem fios",
+    reference: "DJV180Z",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DJV180Z_C2L0.jpg",
+  },
+  {
+    id: 9,
+    name: "Plaina eléctrica",
+    reference: "KP0810",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/KP0810_C2L0.jpg",
+  },
+  {
+    id: 10,
+    name: "Fresadora",
+    reference: "RP1800F",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/RP1800F_C2L0.jpg",
+  },
+  {
+    id: 11,
+    name: "Esmeriladora de bancada",
+    reference: "GB602",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/GB602_C2L0.jpg",
+  },
+  {
+    id: 12,
+    name: "Tupia manual",
+    reference: "3612",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/3612_C2L0.jpg",
+  },
+  {
+    id: 13,
+    name: "Lixadora de fita",
+    reference: "9404",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/9404_C2L0.jpg",
+  },
+  {
+    id: 14,
+    name: "Parafusadora angular",
+    reference: "TL061D",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/TL061D_C2L0.jpg",
+  },
+  {
+    id: 15,
+    name: "Cortadora de azulejos",
+    reference: "4100NH",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/4100NH_C2L0.jpg",
+  },
+  {
+    id: 16,
+    name: "Chave de fendas eléctrica",
+    reference: "DFS452Z",
+    imageUrl:
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DFS452Z_C2L0.jpg",
+  },
 ];
