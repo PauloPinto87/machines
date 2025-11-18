be8d92bc7c45638c64fe800867a64636d6cea9db
PauloPinto87 <paulo@example.com>
2025-11-18 22:45:18 +0000

    Arrumada imagens das maquinas

--- src/data/machines.js
+++ src/data/machines.js
@@ -6,55 +6,55 @@ export const machines = [
     reference: "HP001GZ",
     name: "Berbequim combinado",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/TD004G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/HP001G_C2L0.jpg",
   },
   {
     id: 2,
-    reference: "DF001GZ",
+    reference: "DF001GZ", //Revisar no catalogo XGT a sua medida
     name: "Berbequim aparafusador",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/DF001G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DF001G_C2L0.jpg",
   },
   {
     id: 3,
-    reference: "DA001GZ",
+    reference: "DA001GZ", //Revisar no catalogo XGT a sua medida
     name: "Berbequim angular",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/DA001G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DA001G_C2L0.jpg",
   },
   {
     id: 4,
     reference: "TD003GZ",
     name: "Parafusadora de impacto",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/TD003G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/TD003G_C2L0.jpg",
   },
   {
     id: 5,
     reference: "TW010GZ",
     name: "Chave de impacto",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/TW010G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/TW010G_C2L0.jpg",
   },
   {
     id: 6,
     reference: "TW011GZ",
     name: "Chave de impacto",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/TW011G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/TW011G_C2L0.jpg",
   },
   {
     id: 7,
     reference: "HR006GZ",
     name: "Martelo combinado",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/HR006G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/HR006G_C2L0.jpg",
   },
   {
     id: 8,
     reference: "HM002GZ03",
     name: "Martelo demolidor",
     imageUrl:
-      "https://makita.com.sg/wp-content/uploads/2025/04/HR008G-300x283.jpg",
+      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/HR008G_C2L0.jpg",
   }
 ];
