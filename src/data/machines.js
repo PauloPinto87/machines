99e0520a90e753062135e85f014b82ad5468098f
Paulo Pinto <paulo.pinto@makita.pt>
2025-11-18 16:26:26 +0000

    Revisado maquinas

--- src/data/machines.js
+++ src/data/machines.js
@@ -3,184 +3,58 @@
 export const machines = [
   {
     id: 1,
-    name: "Aparafusadora de impacto",
-    reference: "TD004G",
+    reference: "HP001GZ",
+    name: "Berbequim combinado",
     imageUrl:
       "https://makita.com.sg/wp-content/uploads/2025/04/TD004G-300x283.jpg",
   },
   {
     id: 2,
-    name: "Chave de impacto",
-    reference: "TW001G",
+    reference: "DF001GZ",
+    name: "Berbequim aparafusador",
     imageUrl:
-      "https://nl.makitamedia.com/afbeeldingen/100_Makita/102_machines/102_1_product/JPG_product/TW001G_C1L0.jpg",
+      "https://makita.com.sg/wp-content/uploads/2025/04/DF001G-300x283.jpg",
   },
   {
     id: 3,
-    name: "Serra circular",
-    reference: "HS7601",
+    reference: "DA001GZ",
+    name: "Berbequim angular",
     imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/HS7601_C2L0.jpg",
+      "https://makita.com.sg/wp-content/uploads/2025/04/DA001G-300x283.jpg",
   },
   {
     id: 4,
-    name: "Rebarbadora angular",
-    reference: "DGA513",
+    reference: "TD003GZ",
+    name: "Parafusadora de impacto",
     imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DGA513_C2L0.jpg",
+      "https://makita.com.sg/wp-content/uploads/2025/04/TD003G-300x283.jpg",
   },
   {
     id: 5,
-    name: "Martelo combinado",
-    reference: "HR2470",
+    reference: "TW010GZ",
+    name: "Chave de impacto",
     imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/HR2470_C2L0.jpg",
+      "https://makita.com.sg/wp-content/uploads/2025/04/TW010G-300x283.jpg",
   },
   {
     id: 6,
-    name: "Lixadora rotorbital",
-    reference: "BO5041",
+    reference: "TW011GZ",
+    name: "Chave de impacto",
     imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/BO5041_C2L0.jpg",
+      "https://makita.com.sg/wp-content/uploads/2025/04/TW011G-300x283.jpg",
   },
   {
     id: 7,
-    name: "Berbequim aparafusador",
-    reference: "DF002G",
+    reference: "HR006GZ",
+    name: "Martelo combinado",
     imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DF002G_C2L0.jpg",
+      "https://makita.com.sg/wp-content/uploads/2025/04/HR006G-300x283.jpg",
   },
   {
     id: 8,
-    name: "Serra de mesa",
-    reference: "2712",
+    reference: "HR007GZ",
+    name: "Martelo combinado",
     imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/2712_C2L0.jpg",
-  },
-  {
-    id: 9,
-    name: "Lixadora rotorbital",
-    reference: "BO5030",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/BO5030_C2L0.jpg",
-  },
-  {
-    id: 10,
-    name: "Mini-rebarbadora",
-    reference: "GA5030R",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/GA5030R_C2L0.jpg",
-  },
-  {
-    id: 11,
-    name: "Berbequim",
-    reference: "DP4003",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DP4003_C2L0.jpg",
-  },
-  {
-    id: 12,
-    name: "Aparafusadora",
-    reference: "DFS451",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DFS451_C2L0.jpg",
-  },
-  {
-    id: 13,
-    name: "Martelo ligeiro",
-    reference: "DHR182",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DHR182_C2L0.jpg",
-  },
-  {
-    id: 14,
-    name: "Chave de impacto",
-    reference: "TW007G",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/TW007G_C2L0.jpg",
-  },
-  {
-    id: 15,
-    name: "Serra tico-tico",
-    reference: "DJV186",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DJV186_C2L0.jpg",
-  },
-  {
-    id: 16,
-    name: "Tupia",
-    reference: "RP1111C",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/RP1111C_C2L0.jpg",
-  },
-  {
-    id: 17,
-    name: "Lixadora orbital",
-    reference: "DBO480",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DBO480_C2L0.jpg",
-  },
-  {
-    id: 18,
-    name: "Aspirador de pó",
-    reference: "VC3011L",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/VC3011L_C2L0.jpg",
-  },
-  {
-    id: 19,
-    name: "Motosserra",
-    reference: "UC025G",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/UC025G_C2L0.jpg",
-  },
-  {
-    id: 20,
-    name: "Motosserra",
-    reference: "KP312S",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/KP312S_C2L0.jpg",
-  },
-  {
-    id: 21,
-    name: "Berbequim combinado",
-    reference: "DHP484",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/DHP484_C2L0.jpg",
-  },
-  {
-    id: 23,
-    name: "Serra de fita portátil",
-    reference: "2107F",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/2107F_C2L0.jpg",
-  },
-  {
-    id: 24,
-    name: "Motosserra",
-    reference: "UC4050A",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/UC4050A_C2L0.jpg",
-  },
-  {
-    id: 25,
-    name: "Lixadora de fita",
-    reference: "9403",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/9403_C2L0.jpg",
-  },
-  {
-    id: 26,
-    name: "Roçadora ",
-    reference: "UR003G",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/UR003G_C2L0.jpg",
-  },
-  {
-    id: 27,
-    name: "Corta-relvas",
-    reference: "LM002G",
-    imageUrl:
-      "https://fi.makitamedia.com/images/3_Makita/301_machines/3011_a_GS1/30120_JPG_zoom/LM002G_C2L0.jpg",
-  },
+      "https://makita.com.sg/wp-content/uploads/2025/04/HR007G-300x283.jpg",
+  }
 ];
